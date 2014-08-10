9ee6796731b7e36d569d7bb381754c5b0f9bddb9
kozan <kozan@users.noreply.github.com>
2014-08-11 01:07:59 +0200

    Solution and two empty projects

---
+++ src/AssertStreamliner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assert Streamliner")]
+[assembly: AssemblyDescription("Provides helper methods for code using 'code contracts' approach via Debug.Assert")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Piotr Kozanowski")]
+[assembly: AssemblyProduct("Assert Streamliner")]
+[assembly: AssemblyCopyright("Copyright © 2014 by Piotr Kozanowski")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdd169dd-a597-4603-94df-d60de73647eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

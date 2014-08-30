24708a656f1b97ec6622318536dbddf828feb679
kozan <kozan@users.noreply.github.com>
2014-08-31 00:20:58 +0200

    Added implementation of ICodeContractViolationNotifier using exceptions - ExceptionNotifier

---
+++ src/AssertStreamliner/CodeContractViolationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AssertStreamliner
+{
+	public class CodeContractViolationException : Exception
+	{
+		#region Constructors
+		
+		public CodeContractViolationException(CodeContractViolationData violationData)
+			: base(violationData.ErrorMessage)
+		{
+			this.Value = violationData.Value;
+			this.Name = violationData.Name;
+		}
+
+		#endregion Constructors
+
+		#region Properties
+		
+		public object Value { get; private set; }
+
+		public string Name { get; private set; }
+
+		#endregion Properties
+	}
+}
